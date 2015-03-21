84c722b91f31ba276e2b905d293f6448e0002404
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-03-21 09:02:29 -0400

    UI changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,7 @@ class ProjectsController < ApplicationController
     criteria[:Location] = params[:location]
     criteria[:Keywords] = params[:keywords]
     @job_results = Cb.job.search criteria
-#binding.pry #Cannot be the last line of executed code
+    #binding.pry #Cannot be the last line of executed code
 
     #@job_results = search
     #@results = Project.search params[:keywords]
